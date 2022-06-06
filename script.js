33860fdb2961cbdbc298e046a2e5488d534263c2
dlhardi <derricklhardison@gmail.com>
2022-06-06 18:57:18 -0400

    push

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("attached");
+// VARIABLES
+
+let myLeads = [];
+
+// DOM VARIABLES
+const saveBtn = document.getElementById("input-button");
+const inputEl = document.getElementById("input-el");
+const ulEl = document.getElementById("ul-el");
+
+// FUNCTIONS
+
+function renderLeads() {
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += `
+    <li>
+      <a href=${myLeads[i]} target='_blank'>  ${myLeads[i]}
+      </a>
+    </li>
+    `;
+    console.log(listItems);
+  }
+  ulEl.innerHTML = listItems;
+}
+
+// EVENT LISTENERS
+saveBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  renderLeads();
+});
