466a6d01cc6c6f26df59a7ebbacd32c79c71f876
dlhardi <derricklhardison@gmail.com>
2022-06-06 22:49:42 -0400

    added manifest file

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function renderLeads() {
   for (let i = 0; i < myLeads.length; i++) {
     listItems += `
     <li>
-      <a href=${myLeads[i]} target='_blank'>  ${myLeads[i]}
+      <a href=${myLeads[i]} target='_blank'>${myLeads[i]}
       </a>
     </li>
     `;
